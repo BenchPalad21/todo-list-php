6a654c3c9495bb542ff07296993e02bdecbb0be5
BenchPalad21 <benchpatrick21@gmail.com>
2025-03-20 21:56:23 +0800

    db.php

--- db.php
+++ db.php
@@ -4,15 +4,12 @@ $username = "root";
 $password = "";
 $dbname = "todo_list";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Create table if not exists
 $sql = "CREATE TABLE IF NOT EXISTS tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     task VARCHAR(255) NOT NULL,
